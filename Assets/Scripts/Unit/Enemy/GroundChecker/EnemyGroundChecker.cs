2054853bb03530f1e5705eaae2d18057cdfbae18
Extelder <Nikolaoskas15@gmail.com>
2025-02-24 20:00:35 +0700

    fix fpv death

--- Assets/Scripts/Unit/Enemy/GroundChecker/EnemyGroundChecker.cs
+++ Assets/Scripts/Unit/Enemy/GroundChecker/EnemyGroundChecker.cs
@@ -9,19 +9,11 @@ public class EnemyGroundChecker : MonoBehaviour
 
     public event Action GroundDetected;
 
-    private void OnCollisionEnter(Collision other)
+    private void OnCollisionStay(Collision other)
     {
         if (other.gameObject.TryGetComponent<Ground>(out Ground ground) || other.gameObject.TryGetComponent<Wall>(out Wall wall))
         {
             Detected = true;
-        }
-    }
-
-    private void OnCollisionExit(Collision other)
-    {
-        if (other.gameObject.TryGetComponent<Ground>(out Ground ground) || other.gameObject.TryGetComponent<Wall>(out Wall wall))
-        {
-            Detected = false;
         }
     }
 }
