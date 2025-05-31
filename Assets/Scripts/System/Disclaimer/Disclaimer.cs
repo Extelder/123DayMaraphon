438e7babbcb1c57a0d18b2c0276cfe89e8a8f729
igoryava <126491702+igoryava@users.noreply.github.com>
2025-05-31 18:23:07 +0300

    fixed disclaimer

--- Assets/Scripts/System/Disclaimer/Disclaimer.cs
+++ Assets/Scripts/System/Disclaimer/Disclaimer.cs
@@ -2,18 +2,35 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UniRx;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Disclaimer : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _text;
+    [SerializeField] private GameObject _skipText;
+    [SerializeField] private Transform _cameraTransform;
     [SerializeField, TextAreaAttribute] private string _disclaimer;
     [SerializeField] private float _cooldown;
 
+    private CompositeDisposable _disposable = new CompositeDisposable();
     private void Start()
     {
         StartCoroutine(SwitchDisclaimer());
+        if (PlayerPrefs.GetInt("DisclaimerComplete", 0) == 1)
+        {
+            _skipText.SetActive(true);
+            Observable.EveryUpdate().Subscribe(_ =>
+            {
+                if (Input.anyKeyDown)
+                {
+                    _cameraTransform.rotation = Quaternion.Euler(0, 0, 0);
+                    StopAllCoroutines();
+                    gameObject.SetActive(false);
+                }
+            }).AddTo(_disposable);
+        }
     }
 
     private IEnumerator SwitchDisclaimer()
@@ -21,6 +38,13 @@ public class Disclaimer : MonoBehaviour
         yield return new WaitForSeconds(_cooldown);
         _text.text = _disclaimer;
         yield return new WaitForSeconds(_cooldown);
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        PlayerPrefs.SetInt("DisclaimerComplete", 1);
+        _cameraTransform.rotation = Quaternion.Euler(0, 0, 0);
+        gameObject.SetActive(false);
+    }
+
+    private void OnDisable()
+    {
+        _disposable.Clear();
     }
 }
