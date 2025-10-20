c749b88af2c958eafb9369abd167eb5ff6ee9c9a
Extelder <Nikolaoskas15@gmail.com>
2025-10-20 21:35:02 +0700

    added new hints, fix hud

--- Assets/Scripts/System/Hints/HintTrigger.cs
+++ Assets/Scripts/System/Hints/HintTrigger.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using Zenject;
 
 public class HintTrigger : PlayerTrigger
 {
+    [SerializeField] private TextMeshProUGUI _text;
+    
     [SerializeField] private string _hintText;
     [Inject] private PlayerCharacter _character;
     public override void OnTriggered()
     { 
+        _text.text = _hintText;
         _character.Hints.AcceptNewHint(_hintText);
     }
 }
